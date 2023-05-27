86dce7128ae2a14c2806289e8a5c08d7e2f55f5b
konstantin.af <ovsanka88@mail.ru>
2023-05-27 18:11:19 +0300

    second commit

--- test_items.py
+++ test_items.py
@@ -1,11 +1,9 @@
-import time
 from selenium.webdriver.common.by import By
-
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_search_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
-    time.sleep(5)
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg")
+    assert len(button) > 0, "Кнопки нет"
